dae184de73c8c37c8e44c59a2eb59508e460795c
Yaroslav <lordrom92@gmail.com>
2026-07-10 14:43:40 +0300

    фыв

---
+++ src/main/java/com/taskspace/userservice/config/minio/props/MinioProperties.java
@@ -0,0 +1,12 @@
+package com.taskspace.userservice.config.minio.props;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "minio")
+public record MinioProperties(
+        String endpoint,
+        String accessKey,
+        String secretKey,
+        String bucket
+) {
+}
